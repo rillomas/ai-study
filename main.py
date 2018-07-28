08f61dc9ad85fadc68cdfe3181fc810ea60ef9b4
Masato Hori <rillomas@gmail.com>
2018-07-28 16:50:46 +0900

    added xml parsing

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python
+from lxml import etree as et
+import click
 
+@click.command()
+@click.option('--path','-p',default=None)
 def parse_xml(path):
     print("Parsing xml {}".format(path))
+    tree = et.parse(path)
+    sentences = tree.findall(".//Sentence")
+    print(len(sentences))
+    for s in sentences:
+        print(s.text)
 
 if __name__ == "__main__":
-    parse_xml("data/")
+    parse_xml()
 
