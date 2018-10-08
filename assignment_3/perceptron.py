ad7e532e958972515e585e0e73e62d59d295809f
Masato Hori <rillomas@gmail.com>
2018-10-08 12:50:21 +0900

    implemented XOR using NOR and NAND

--- assignment_3/perceptron.py
+++ assignment_3/perceptron.py
@@ -1,18 +1,53 @@
 import numpy as np
 import pdb
 
-def NOR(l, r):
+def AND(l, r):
     x = np.array([l, r])
     w = np.array([0.5, 0.5])
-    b = -0.3
+    b = -0.7
     s = np.sum(w*x) + b
-    return 0 if s >= 0 else 1
+    return 0 if s <= 0 else 1
+
+def NAND(l, r):
+    x = np.array([l, r])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    s = np.sum(w*x) + b
+    return 0 if s <= 0 else 1
+
+def OR(l, r):
+    x = np.array([l, r])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    s = np.sum(w*x) + b
+    return 0 if s <= 0 else 1
+
+def NOR(l, r):
+    x = np.array([l, r])
+    w = np.array([-0.5, -0.5])
+    b = 0.3
+    s = np.sum(w*x) + b
+    return 0 if s <= 0 else 1
 
 def XOR(l, r):
-    #ll = NOR(l, r)
-    return 0
+    ll = OR(l, r)
+    rr = NAND(l, r)
+    return AND(ll, rr)
+
+def XOR2(l, r):
+    ll = NOR(l, r)
+    rr = AND(l, r)
+    return NOR(ll, rr)
 
 if __name__ == "__main__":
+    assert AND(0,0) is 0
+    assert AND(1,0) is 0
+    assert AND(0,1) is 0
+    assert AND(1,1) is 1
+    assert OR(0,0) is 0
+    assert OR(1,0) is 1
+    assert OR(0,1) is 1
+    assert OR(1,1) is 1
     assert NOR(0,0) is 1
     assert NOR(1,0) is 0
     assert NOR(0,1) is 0
@@ -21,4 +56,8 @@ if __name__ == "__main__":
     assert XOR(1,0) is 1
     assert XOR(0,1) is 1
     assert XOR(1,1) is 0
+    assert XOR2(0,0) is 0
+    assert XOR2(1,0) is 1
+    assert XOR2(0,1) is 1
+    assert XOR2(1,1) is 0
 
