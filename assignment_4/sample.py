ffc962264b59a9fa0e182d82ab844ba5c4dba697
Masato Hori <rillomas@gmail.com>
2018-10-28 12:45:46 +0900

    fixed style warnings

--- assignment_4/sample.py
+++ assignment_4/sample.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
+
 import torch
 
 if __name__ == "__main__":
-	x = torch.rand(5, 3)
-	print(x)
-	print("CUDA available:", torch.cuda.is_available())
-	device = 0
-	print(torch.cuda.get_device_capability(device))
-	print(torch.cuda.get_device_name(device))
+    x = torch.rand(5, 3)
+    print(x)
+    print("CUDA available:", torch.cuda.is_available())
+    device = 0
+    print(torch.cuda.get_device_capability(device))
+    print(torch.cuda.get_device_name(device))
