1998a8b84d45cf519acb19272192fa125b759602
Masato Hori <rillomas@gmail.com>
2018-12-22 17:38:52 +0900

    adding comments

--- assignment_5/reader.py
+++ assignment_5/reader.py
@@ -51,6 +51,7 @@ def _build_vocab(filename):
 
 
 def _file_to_word_ids(filename, word_to_id):
+  """Convert the whole word sequence to IDs"""
   data = _read_words(filename)
   return [word_to_id[word] for word in data if word in word_to_id]
 
@@ -105,14 +106,23 @@ def ptb_producer(raw_data, batch_size, num_steps, name=None):
   Raises:
     tf.errors.InvalidArgumentError: if batch_size or num_steps are too high.
   """
+  import pdb
+  pdb.set_trace()
   with tf.name_scope(name, "PTBProducer", [raw_data, batch_size, num_steps]):
+    # batch_size == 20
+    # num_steps == 2
+    # raw_data = 1 dimension array of size 929589 (train_data)
     raw_data = tf.convert_to_tensor(raw_data, name="raw_data", dtype=tf.int32)
 
     data_len = tf.size(raw_data)
-    batch_len = data_len // batch_size
+    batch_len = data_len // batch_size  # 929589 // 20 = 46479
+    # reshape the input data to multiple batches
+    # so that each batch has batch_len elements
+    # data.shape == (29, 46479)
     data = tf.reshape(raw_data[0 : batch_size * batch_len],
                       [batch_size, batch_len])
 
+    # epoch_size == (46479 - 1) // 2
     epoch_size = (batch_len - 1) // num_steps
     assertion = tf.assert_positive(
         epoch_size,
@@ -121,7 +131,6 @@ def ptb_producer(raw_data, batch_size, num_steps, name=None):
       epoch_size = tf.identity(epoch_size, name="epoch_size")
 
     i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
-    #i = tf.data.Dataset.range(epoch_size).dequeue()
     x = tf.strided_slice(data, [0, i * num_steps],
                          [batch_size, (i + 1) * num_steps])
     x.set_shape([batch_size, num_steps])
