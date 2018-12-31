b892bfec2df5a19dc361a58f5436005d69d4aebd
Masato Hori <rillomas@gmail.com>
2018-12-31 11:17:06 +0900

    fixed style by yapf

--- assignment_5/reader.py
+++ assignment_5/reader.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
-
 """Utilities for parsing PTB text files."""
 from __future__ import absolute_import
 from __future__ import division
@@ -27,37 +25,38 @@ import tensorflow as tf
 
 Py3 = sys.version_info[0] == 3
 
+
 def _read_words(filename):
-  with tf.gfile.GFile(filename, "r") as f:
-    if Py3:
-      return f.read().replace("\n", "<eos>").split()
-    else:
-      return f.read().decode("utf-8").replace("\n", "<eos>").split()
+    with tf.gfile.GFile(filename, "r") as f:
+        if Py3:
+            return f.read().replace("\n", "<eos>").split()
+        else:
+            return f.read().decode("utf-8").replace("\n", "<eos>").split()
 
 
 def _build_vocab(filename):
-  """Read all vocabularies from text file and generate a word-to-id
+    """Read all vocabularies from text file and generate a word-to-id
   dictionary which has the most frequent words with a lower IDs and rare words
   as higher IDs"""
-  data = _read_words(filename)
+    data = _read_words(filename)
 
-  counter = collections.Counter(data)
-  count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+    counter = collections.Counter(data)
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
 
-  words, _ = list(zip(*count_pairs))
-  word_to_id = dict(zip(words, range(len(words))))
+    words, _ = list(zip(*count_pairs))
+    word_to_id = dict(zip(words, range(len(words))))
 
-  return word_to_id
+    return word_to_id
 
 
 def _file_to_word_ids(filename, word_to_id):
-  """Convert the whole word sequence to IDs"""
-  data = _read_words(filename)
-  return [word_to_id[word] for word in data if word in word_to_id]
+    """Convert the whole word sequence to IDs"""
+    data = _read_words(filename)
+    return [word_to_id[word] for word in data if word in word_to_id]
 
 
 def ptb_raw_data(data_path=None):
-  """Load PTB raw data from data directory "data_path".
+    """Load PTB raw data from data directory "data_path".
 
   Reads PTB text files, converts strings to integer ids,
   and performs mini-batching of the inputs.
@@ -75,20 +74,20 @@ def ptb_raw_data(data_path=None):
     where each of the data objects can be passed to PTBIterator.
   """
 
-  train_path = os.path.join(data_path, "ptb.train.txt")
-  valid_path = os.path.join(data_path, "ptb.valid.txt")
-  test_path = os.path.join(data_path, "ptb.test.txt")
+    train_path = os.path.join(data_path, "ptb.train.txt")
+    valid_path = os.path.join(data_path, "ptb.valid.txt")
+    test_path = os.path.join(data_path, "ptb.test.txt")
 
-  word_to_id = _build_vocab(train_path)
-  train_data = _file_to_word_ids(train_path, word_to_id)
-  valid_data = _file_to_word_ids(valid_path, word_to_id)
-  test_data = _file_to_word_ids(test_path, word_to_id)
-  vocabulary = len(word_to_id)
-  return train_data, valid_data, test_data, vocabulary
+    word_to_id = _build_vocab(train_path)
+    train_data = _file_to_word_ids(train_path, word_to_id)
+    valid_data = _file_to_word_ids(valid_path, word_to_id)
+    test_data = _file_to_word_ids(test_path, word_to_id)
+    vocabulary = len(word_to_id)
+    return train_data, valid_data, test_data, vocabulary
 
 
 def ptb_producer(raw_data, batch_size, num_steps, name=None):
-  """Iterate on the raw PTB data.
+    """Iterate on the raw PTB data.
 
   This chunks up raw_data into batches of examples and returns Tensors that
   are drawn from these batches.
@@ -106,33 +105,34 @@ def ptb_producer(raw_data, batch_size, num_steps, name=None):
   Raises:
     tf.errors.InvalidArgumentError: if batch_size or num_steps are too high.
   """
-  with tf.name_scope(name, "PTBProducer", [raw_data, batch_size, num_steps]):
-    # batch_size == 20
-    # num_steps == 2
-    # raw_data = 1 dimension array of size 929589 (train_data)
-    raw_data = tf.convert_to_tensor(raw_data, name="raw_data", dtype=tf.int32)
+    with tf.name_scope(name, "PTBProducer", [raw_data, batch_size, num_steps]):
+        # batch_size == 20
+        # num_steps == 2
+        # raw_data = 1 dimension array of size 929589 (train_data)
+        raw_data = tf.convert_to_tensor(
+            raw_data, name="raw_data", dtype=tf.int32)
 
-    data_len = tf.size(raw_data)
-    batch_len = data_len // batch_size  # 929589 // 20 = 46479
-    # reshape the input data to multiple batches
-    # so that each batch has batch_len elements
-    # data.shape == (20, 46479)
-    data = tf.reshape(raw_data[0 : batch_size * batch_len],
-                      [batch_size, batch_len])
+        data_len = tf.size(raw_data)
+        batch_len = data_len // batch_size  # 929589 // 20 = 46479
+        # reshape the input data to multiple batches
+        # so that each batch has batch_len elements
+        # data.shape == (20, 46479)
+        data = tf.reshape(raw_data[0:batch_size * batch_len],
+                          [batch_size, batch_len])
 
-    # epoch_size == (46479 - 1) // 2
-    epoch_size = (batch_len - 1) // num_steps
-    assertion = tf.assert_positive(
-        epoch_size,
-        message="epoch_size == 0, decrease batch_size or num_steps")
-    with tf.control_dependencies([assertion]):
-      epoch_size = tf.identity(epoch_size, name="epoch_size")
+        # epoch_size == (46479 - 1) // 2
+        epoch_size = (batch_len - 1) // num_steps
+        assertion = tf.assert_positive(
+            epoch_size,
+            message="epoch_size == 0, decrease batch_size or num_steps")
+        with tf.control_dependencies([assertion]):
+            epoch_size = tf.identity(epoch_size, name="epoch_size")
 
-    i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
-    x = tf.strided_slice(data, [0, i * num_steps],
-                         [batch_size, (i + 1) * num_steps])
-    x.set_shape([batch_size, num_steps])
-    y = tf.strided_slice(data, [0, i * num_steps + 1],
-                         [batch_size, (i + 1) * num_steps + 1])
-    y.set_shape([batch_size, num_steps])
-    return x, y
+        i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
+        x = tf.strided_slice(data, [0, i * num_steps],
+                             [batch_size, (i + 1) * num_steps])
+        x.set_shape([batch_size, num_steps])
+        y = tf.strided_slice(data, [0, i * num_steps + 1],
+                             [batch_size, (i + 1) * num_steps + 1])
+        y.set_shape([batch_size, num_steps])
+        return x, y
