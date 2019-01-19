05f907b2b90c0145e3004f940e03804818cc6c22
Masato Hori <rillomas@gmail.com>
2019-01-19 17:03:31 +0900

    added basic code from sample

--- assignment_6/titanic.py
+++ assignment_6/titanic.py
@@ -1,39 +1,84 @@
 import pandas as pd
-
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 def preprocess_data(data):
-    # print(data.describe())
-    print(data.shape)
-    print(data.head())
-    print(data.isnull().sum())
+    """Common preprocessing for both
+    training and testing data
+    """
+    df_sex = pd.get_dummies(data['Sex'])
+    df = pd.concat((data, df_sex), axis=1)
     drop_col = [
         'Name',
         'Ticket',
         'Cabin',
-        'Embarked'
+        'Embarked',
+        'Sex',
+        'Age'
     ]
-    output = data.drop(columns=drop_col)
-    print(output.isnull().sum())
-    print(output.head())
-    return output
+    leftover = df.drop(columns=drop_col)
+    return leftover
 
 
-def train(data):
-    model = "aardvark"
-    return model
+def preprocess_train_data(data):
+    # print(data.describe())
+    print(data.shape)
+    print(data.head())
+    print(data.isnull().sum())
+    leftover = preprocess_data(data)
+    cols = list(leftover.columns)
+    cols.remove('Survived')
+    print(leftover[cols].head())
+    x = np.array(leftover[cols], dtype=np.float32)
+    y = np.array(leftover['Survived'], dtype=np.int32)
+    print(x)
+    print(len(y))
+    return (x, y)
+
+
+def preprocess_test_data(data):
+    leftover = preprocess_data(data)
+    # Fill the empty fare with a mean value
+    col = "Fare"
+    colval = leftover[col]
+    leftover[col] = colval.fillna(colval.mean())
+    # print(leftover.isnull().sum())
+    cols = list(leftover.columns)
+    # print(leftover[cols].head())
+    x = np.array(leftover[cols], dtype=np.float32)
+    return x
+
+
+def train(data, label):
+    clf = RandomForestClassifier()
+    clf.fit(data, label)
+    return clf
 
 
 def test(model, data):
-    print(model, data.head())
-    result = "giraffe"
-    return result
+    return model.predict(data)
+
+
+def calc_ratio(model, data, label):
+    result = model.predict(data)
+    assert len(label) == len(result)
+    # ratio = 0.0
+    sumval = sum(exp == act for exp, act in zip(label, result))
+    return sumval / len(label)
+    # for exp, act in zip(label, result):
+    #     if exp == act
+    # return ratio
 
 
 if __name__ == "__main__":
     train_data = pd.read_csv("train.csv")
-    # test_data = pd.read_csv("test.csv")
-    processed_train_data = preprocess_data(train_data)
-    # processed_test_data = preprocess_data(test_data)
-    # model = train(processed_train_data)
-    # result = test(model, processed_test_data)
-    # print(result)
+    pp_data, label = preprocess_train_data(train_data)
+    model = train(pp_data, label)
+    print(model)
+    test_data = pd.read_csv("test.csv")
+    ppt_data = preprocess_test_data(test_data)
+    print(ppt_data)
+    result = test(model, ppt_data)
+    print(result)
+    ratio = calc_ratio(model, pp_data, label)
+    print(ratio)
